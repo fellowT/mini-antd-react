c4a72fbe736c97856cecd921536fbac66632a095
tangxianwen <1658172479@qq.com>
2020-10-26 17:58:46 +0800

    feat:开发button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button,{ButtonType,ButtonSize} from './components/Button/button'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button onClick={(e)=> {e.preventDefault();alert(123)}} className="custom">Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large} disabled> Disabled Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger</Button>
+        <Button  btnType={ButtonType.Link} href='http://ww.baidu.link' > Baidu Link</Button>
+        <Button disabled btnType={ButtonType.Link} href='http://ww.baidu.link' > Disabled Link</Button>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <hr/>
+        <code>
+          const a = 'b'
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
